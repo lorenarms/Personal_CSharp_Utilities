d18fa6080f1432971a6ed0687c5b9820b362c4ba
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-10-07 13:35:50 -0700

    Update

---
+++ PrivateInputEngine/PrivateInput.cs
@@ -0,0 +1,68 @@
+﻿//============================================================================
+// Name        : PRIVATE INPUT
+// Author      : Lorenzo Dominguez
+// Version     : 1.1.0
+// Copyright   : Copyright © October 7, 2021
+// Description : Input a string privately
+//============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PrivateInputEngine
+{
+    class PrivateInput
+    {
+        private string _privateString;
+
+        public string GetPrivateString()
+        {
+            return _privateString;
+        }
+
+        public void InputPrivately()
+        {
+            Console.Write("Type in your message privately: ");
+
+            string tempString = "";
+            do
+            {
+                // reads key presses from input
+                ConsoleKeyInfo key = Console.ReadKey(true); 
+
+                // check for backspace or enter key
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter) 
+                {
+
+                    // add character to temporary string
+                    tempString += key.KeyChar;
+
+                    //write a * for the character for secrecy
+                    Console.Write("*"); 
+                }
+                else
+                {
+                    // backspace is pressed and the string has characters in it
+                    if (key.Key == ConsoleKey.Backspace && tempString.Length > 0) 
+                    {
+                        // reduce the temporary string by one character each time bs is pressed
+                        tempString = tempString.Substring(0, (tempString.Length - 1));
+                        
+                        // removes a '*' from the end of the output
+                        Console.Write("\b \b");
+                        
+                    }
+                    else if (key.Key == ConsoleKey.Enter) { break; }
+                }
+            } while (true);
+
+            _privateString = tempString;
+            tempString = null;
+            Console.WriteLine();
+            
+            return;
+        }
+    }
+}
---
+++ PrivateInputEngine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using static System.Console;
+
+
+namespace PrivateInputEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // make private input object for storing private string
+            var privateInput01 = new PrivateInput();
+            var privateInput02 = new PrivateInput();
+
+            // input and store the private string
+            privateInput01.InputPrivately();
+            privateInput02.InputPrivately();
+
+            Clear();
+            
+            // print the private string
+
+
+            Console.WriteLine("Private string: " + privateInput01.GetPrivateString());
+            Console.WriteLine("Private string: " + privateInput02.GetPrivateString());
+            
+            
+            ReadKey();
+        }
+
+    }
+}
+
