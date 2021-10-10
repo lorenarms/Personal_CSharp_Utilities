e6f86207791e9fe46cc9ad06d4307ef4b70d7db1
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-10-10 16:32:35 -0700

    TitleDraw

---
+++ TitleDraw/TitleDraw.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitleDraw
+{
+    class TitleDraw
+    {
+        private string _titleToDraw;
+        private int _titleWidth;
+        private int _titleStartColumn;
+        
+        public void DrawTitle(string title)
+        {
+
+        }
+
+        public void SetWidth(string title)
+        {
+
+        }
+    }
+}
---
+++ TitleDraw/WindowUtilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TitleDraw
+{
+    class WindowUtilities
+    {
+        public static void SetConsoleWindowSize(int width, int height)
+        {
+            WindowWidth = width;
+            WindowHeight = height;
+        }
+
+        public static int GetConsoleWindowWidth()
+        {
+            return WindowWidth;
+        }
+
+        public static void SetConsoleTextColor(ConsoleColor foreground, ConsoleColor background)
+        {
+            ForegroundColor = foreground;
+            BackgroundColor = background;
+        }
+
+        public static void ResetCursorVisible()
+        {
+            CursorVisible = CursorVisible != true;
+        }
+
+        public static void SetCursorPosition(int row, int column)
+        {
+            if (row > 0 && row < WindowHeight)
+            {
+                CursorTop = row;
+            }
+
+            if (column > 0 && column < WindowWidth)
+            {
+                CursorLeft = column;
+            }
+        }
+    }
+}
