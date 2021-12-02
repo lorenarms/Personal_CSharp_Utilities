5cb986c8ac97c2ec00be38835e8823ed77382fa3
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-12-02 07:22:47 -0800

    generate unique id

---
+++ RandomIDGenerator/Driver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomIDGenerator
+{
+    class Driver
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable t = new Hashtable();
+
+            
+            
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Object o = new object();
+                String id = GenerateID.generateUniqueID();
+                while (t.Contains(id))
+                {
+                    id = GenerateID.generateUniqueID();
+                }
+                
+                t.Add(GenerateID.generateUniqueID(), o);
+                Console.WriteLine("Hash Table count: " + t.Count);
+                Thread.Sleep(1);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ RandomIDGenerator/GenerateID.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomIDGenerator
+{
+    class GenerateID
+    {
+        const int ID_SIZE = 10;
+        private static string[] _pool;
+
+        private static void generatePool()
+        {
+            _pool = new string[62];
+            int index = 0;
+            for (int i = 48; i < 58; i++)
+            {
+                _pool[index] = Char.ToString((char) i);
+                index++;
+            }
+
+            for (int i = 65; i < 91; i++)
+            {
+                _pool[index] = Char.ToString((char) i);
+                index++;
+            }
+
+            for (int i = 97; i < 123; i++)
+            {
+                _pool[index] = Char.ToString((char) i);
+                index++;
+            }
+        }
+
+        public static string generateUniqueID()
+        {
+            if (_pool == null)
+            {
+                generatePool();
+            }
+
+            string s = "";
+            Random rnd = new Random();
+            for (int i = 0; i < ID_SIZE; i++)
+            {
+                s = s + _pool[rnd.Next(_pool.Length)];
+            }
+
+            return s;
+        }
+    }
+}
