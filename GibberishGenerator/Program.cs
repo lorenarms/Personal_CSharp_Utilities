5fc2191d4da2b855bfcc72dc6c7534e42d1abab8
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-12-25 17:02:06 -0800

    added Gibberish Generator project

---
+++ GibberishGenerator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GibberishGenerator
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //TODO: add option for generating numbers or letters (capital or lowercase)
+            //TODO: add options for length of lines and number of lines
+
+            var generator = new RandomGenerator();
+            var writeLines = new WriteAllAlines();
+
+            List<string> linesToWriteToFile = new List<string> { };
+
+            
+            for (int i = 0; i < 20; i++)
+            {
+                var randomString = generator.RandomString(40);
+                linesToWriteToFile.Add(randomString);
+            }
+
+            //TODO: add option to name file
+
+            Task writeToFile = writeLines.WriteStringsToFile(linesToWriteToFile);
+
+            //TODO: add loop to make a new file if desired
+        }
+    }
+
+    public class RandomGenerator
+    {
+        // Instantiate random number generator.  
+        // It is better to keep a single Random instance 
+        // and keep using Next on the same instance.  
+        private readonly Random _random = new Random();
+
+        // Generates a random number within a range.      
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        // Generates a random string with a given size.    
+        public string RandomString(int size, bool lowerCase = true)
+        {
+            var builder = new StringBuilder(size);
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):   
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length = 26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+    }
+
+    class WriteAllAlines
+    {
+        public async Task WriteStringsToFile(List<string> input)
+        {
+           
+            // writes a file to the current program directory, wherever that is
+            await File.WriteAllLinesAsync(@".\RandomTextWritten.txt", input);
+            
+        }
+    }
+}
+
+
--- PatternFinder/StringAndPattern.cs
+++ PatternFinder/StringAndPattern.cs
@@ -19,26 +19,26 @@ namespace PatternFinder
 {
     class StringAndPattern
     {
-        private readonly string[] _str;
-        private readonly string[] _pat;
-        private readonly int _strHeight;
-        private readonly int _strWidth;
-        private readonly int _patHeight;
-        private readonly int _patWidth;
+        private readonly string[] _pictureAsString;
+        private readonly string[] _patternAsString;
+        private readonly int _heightOfPictureAsString;
+        private readonly int _widthOfPictureAsString;
+        private readonly int _heightOfPatternAsString;
+        private readonly int _widthOfPatternAsString;
 
-        private readonly int[] _location;
+        private readonly int[] _locationOfPatternInPicture;
 
-        public StringAndPattern(string[] str, string[] pat)
+        public StringAndPattern(string[] pictureAsString, string[] patternAsString)
         {
-            this._str = str;
-            this._pat = pat;
+            this._pictureAsString = pictureAsString;
+            this._patternAsString = patternAsString;
 
-            _strHeight = str.Length;
-            _strWidth = str[0].Length;
-            _patHeight = pat.Length - 1;
-            _patWidth = pat[0].Length - 1;
+            _heightOfPictureAsString = pictureAsString.Length;
+            _widthOfPictureAsString = pictureAsString[0].Length;
+            _heightOfPatternAsString = patternAsString.Length - 1;
+            _widthOfPatternAsString = patternAsString[0].Length - 1;
 
-            _location = new[] {-1, -1};
+            _locationOfPatternInPicture = new[] {-1, -1};
 
         }
 
@@ -46,39 +46,39 @@ namespace PatternFinder
         {
             
             // flag for if patter is found
-            bool foundPattern = false;
+            bool patternWasFound = false;
 
             // outer loop through 'rows' of main array
-            for (int i = 0; i < _strHeight; i++)
+            for (int i = 0; i < _heightOfPictureAsString; i++)
             {
                 // inner loop for chars of each row
-                for (int j = 0; j < _strWidth; j++)
+                for (int j = 0; j < _widthOfPictureAsString; j++)
                 {
 
                     // look for matching single char first
                     // only finds the first instance (foundPattern stops any others)
                     // doesn't check if the found char is too close to the end of the string
-                    if (_str[i][j] == _pat[0][0] && !foundPattern && (j < _strWidth - _patWidth))
+                    if (_pictureAsString[i][j] == _patternAsString[0][0] && !patternWasFound && (j < _widthOfPictureAsString - _widthOfPatternAsString))
                     {
                         // marker vars
                         int l = 0;
                         int k = i;
 
                         // set a substring starting at row[k] and ending at length of pattern
-                        string tempStr = _str[k].Substring(j, _patWidth + 1);
+                        string tempStr = _pictureAsString[k].Substring(j, _widthOfPatternAsString + 1);
                         
 
                         // while the chars in the substring and the current pattern row match
-                        while (tempStr == _pat[l])
+                        while (tempStr == _patternAsString[l])
                         {
                             // check if 'l' marker equals the number of 'rows' in the pattern
                             // l marker will equal this only if the while condition was true enough times
-                            if (l == _patHeight)
+                            if (l == _heightOfPatternAsString)
                             {
                                 
-                                foundPattern = true;
-                                _location[0] = i;
-                                _location[1] = j;
+                                patternWasFound = true;
+                                _locationOfPatternInPicture[0] = i;
+                                _locationOfPatternInPicture[1] = j;
                                 break;
                             }
 
@@ -87,33 +87,33 @@ namespace PatternFinder
                             k++;
 
                             // update the substring to compare pattern to
-                            tempStr = _str[k].Substring(j, _patWidth + 1);
+                            tempStr = _pictureAsString[k].Substring(j, _widthOfPatternAsString + 1);
                         }
                     }
                 }
             }
             
-            return _location;
+            return _locationOfPatternInPicture;
         }
 
         public void DrawPictureAndPattern()
         {
             WriteLine("PICTURE:");
-            for (int i = 0; i < _str.Length; i++)
+            for (int i = 0; i < _pictureAsString.Length; i++)
             {
-                for (int j = 0; j < _str[0].Length; j++)
+                for (int j = 0; j < _pictureAsString[0].Length; j++)
                 {
-                    Write(_str[i][j]);
+                    Write(_pictureAsString[i][j]);
                 }
                 Write("\n");
             }
 
             WriteLine("\nPATTERN TO FIND:");
-            for (int i = 0; i < _pat.Length; i++)
+            for (int i = 0; i < _patternAsString.Length; i++)
             {
-                for (int j = 0; j < _pat[0].Length; j++)
+                for (int j = 0; j < _patternAsString[0].Length; j++)
                 {
-                    Write(_pat[i][j]);
+                    Write(_patternAsString[i][j]);
                 }
                 Write("\n");
             }
@@ -122,42 +122,42 @@ namespace PatternFinder
 
         public void DrawPatternInPicture()
         {
-            int patLength = _pat.Length;
-            int patHeight = _pat[0].Length;
-            int patRow = _location[0];
+            int patLength = _patternAsString.Length;
+            int patHeight = _patternAsString[0].Length;
+            int patRow = _locationOfPatternInPicture[0];
 
             WriteLine("LOCATION:");
-            for (int i = 0; i < _str.Length; i++)
+            for (int i = 0; i < _pictureAsString.Length; i++)
             {
-                for (int j = 0; j < _str[0].Length; j++)
+                for (int j = 0; j < _pictureAsString[0].Length; j++)
                 {
-                    if (i == _location[0] && j == _location[1])
+                    if (i == _locationOfPatternInPicture[0] && j == _locationOfPatternInPicture[1])
                     {
                         ForegroundColor = ConsoleColor.Red;
                         int k = 0;
-                        while (k < _pat[0].Length)
+                        while (k < _patternAsString[0].Length)
                         {
-                            Write(_str[i][j]);
+                            Write(_pictureAsString[i][j]);
                             k++;
                             j++;
                         }
 
                         ResetColor();
 
-                        if (_location[0] < patLength + patRow - 1)
+                        if (_locationOfPatternInPicture[0] < patLength + patRow - 1)
                         {
-                            _location[0]++;
+                            _locationOfPatternInPicture[0]++;
                         }
 
                         // conditional to stop error of trying to write out of bounds of array
-                        if (j < _str[0].Length)
+                        if (j < _pictureAsString[0].Length)
                         {
-                            Write(_str[i][j]);
+                            Write(_pictureAsString[i][j]);
                         }
                     }
                     else
                     {
-                        Write(_str[i][j]);
+                        Write(_pictureAsString[i][j]);
                     }
                    
 
