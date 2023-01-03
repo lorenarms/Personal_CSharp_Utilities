2e3ad9a7334212675b4665a157dcb25d8c976bb7
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-01-02 19:41:17 -0800

    mongodb access update

--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -10,14 +10,32 @@ namespace MongoDB
     {
         static void Main(string[] args)
         {
+
             // string connectionString = "mongodb://127.0.0.1:27017";
-            string connectionString = "FOUND IN LOG FILE";
             string databaseName = "simple_db";
             string collectionName = "people";
+            
+            //Get username and password
+            WriteLine("Enter the username: ");
+            var userName = ReadLine();
 
+            WriteLine("Enter the password: ");
+            var passWord = ReadLine();
+            //Build connection string and connect to Atlas
+            var connectionString = "mongodb+srv://" + userName + ":" + passWord + "@cluster0.cyqsq.mongodb.net/test";
             MongoClient dbClient = new MongoClient(connectionString);
-
+            Clear();
+            
+            //List databases
             var dbList = dbClient.ListDatabases().ToList();
+            foreach (var db in dbList)
+            {
+                WriteLine("Database Name: " + db);
+            }
+
+            //Select databases
+            WriteLine("\nSelect a Database: ");
+            var database = dbClient.GetDatabase(ReadLine());
 
             //WriteLine("The list of databases on this server is: ");
             //foreach (var db in dbList)
@@ -25,20 +43,32 @@ namespace MongoDB
             //    WriteLine(db);
             //}
 
-            var database = dbClient.GetDatabase("db");
-            var collection = database.GetCollection<BsonDocument>("col");
+            Clear();
+            var collList = database.ListCollectionNames().ToList();
+            foreach (var coll in collList)
+            {
+                WriteLine("Collection Name: " + coll);
+            }
 
-            // var documents = collection.Find(new BsonDocument()).FirstOrDefault();
+            WriteLine("\nSelect a Collection: ");
+            var collection = database.GetCollection<BsonDocument>(ReadLine());
+
+            Clear();
+            //var collection = database.GetCollection<BsonDocument>("col");
+
+            //var documents = collection.Find(new BsonDocument()).FirstOrDefault();
             var documents = collection.Find(new BsonDocument()).ToList();
+
 
             foreach (var document in documents)
             {
-                WriteLine(document.ToString());
+                WriteLine("Document: " + document);
             }
 
-            //WriteLine("\n\nFirst Document: ");
-            //WriteLine(documents.ToString());
-            
+                //WriteLine("\n\nFirst Document: ");
+                //WriteLine(documents.ToString());
+
+
 
         }
     }
