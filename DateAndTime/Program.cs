409f84d68d0af191c74323a6072056716a996fee
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-03-09 08:39:18 -0800

    tiny addtion (date time test)

---
+++ DateAndTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using DateTime = System.DateTime;
+
+namespace DateAndTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+
+            Console.WriteLine(now.DayOfWeek);
+            Console.WriteLine($@"{now.Day:00}/{now:MMMM}/{now.Year:0000}");
+            Console.WriteLine($@"{now.Hour:00}:{now.Minute:00}:{now.Second:00}");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
