0744405ad4694a3d562fb0dd9e57ce8d1332e15d
Lawrence Artl <lorenzo.joseph.dominguez@gmail.com>
2023-03-13 11:27:26 -0700

    SQL input generator developed

---
+++ SQLInputGenerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextWriter
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int count = 3;
+            int start = 0;
+            string file = $@"C:\Users\PanIQ Vegas Office\Desktop\Technician\sqlInsert.txt";
+
+            Console.WriteLine("How many lines to generate?");
+            string? userInput = Console.ReadLine();
+            count = int.Parse(userInput);
+
+            Console.WriteLine("Where to start?");
+            string? userStart = Console.ReadLine();
+            start = int.Parse(userStart);
+
+            var SQLInputGenerator = new SQLInputGenerator();
+
+
+
+            SQLInputGenerator.GeneratInput(start, count);
+
+            List<string> input = SQLInputGenerator.GetInput();
+
+            foreach (string inputItem in input)
+            {
+                //Console.WriteLine(inputItem);
+                File.AppendAllText(file, inputItem);
+            }
+
+            Console.WriteLine("File written successfully!");
+
+        }
+    }
+}
---
+++ SQLInputGenerator/SQLInputGenerator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextWriter
+{
+    public class SQLInputGenerator
+    {
+        private CurrentTime _ct = new CurrentTime(DateTime.Now);
+
+        private static readonly string[] _rooms = new[]
+        {
+            "Wizard", "Casino", "Haunted", "Zombie", "Atlantis", "Time Machine"
+        };
+        private static readonly string[] _day = new[]
+        {
+            "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+        };
+        private static readonly string[] _month = new[]
+        {
+            "January", "February", "March", "April", "May", "June"
+        };
+        private static readonly string[] _year = new[]
+        {
+            "2021", "2022", "2023"        
+        };
+        private static readonly string[] _puzzle = new[]
+        {
+            "Door", "Bench", "Kiosk", "Floor", "Wall", "Table", "Puzzle"
+        };
+        private static readonly string[] _text_used = new[]
+        {
+            "None", "Open the door", "Finish the puzzle", "Turn on the light", "Rotate the knob"
+        };
+        private static readonly string[] _glitch = new[]
+        {
+            "None", "Did not open", "Did not close", "Did not finish", "Did not activate"
+        };
+        private static readonly string[] _bypass = new[]
+        {
+            "None", "Finished manually", "Special message", "Sent custom hint", "Closed room"
+        };
+        private List<string> _input = new List<string>();
+
+
+        public List<string> GetInput()
+        {
+
+            return _input;
+        }
+
+        public void GeneratInput(int start, int i)
+        {
+
+            //INSERT INTO `log` VALUES(1,
+            //'Wizard', 'Thursday', '9', 'March', '2023', '07:40:48',
+            //'Dragon to Cat', '5',
+            //'The cat should appear', 'Cat did not appear',
+            //'Informed players they won');
+
+            for(int j = 0; j < i; j++)
+            {
+                
+                StringBuilder sb = new StringBuilder();
+                sb.Append($@"INSERT INTO `log` VALUES ({start},");
+                sb.Append($@"'{_rooms[Random.Shared.Next(_rooms.Length)]}',");
+                sb.Append($@"'{_day[Random.Shared.Next(_day.Length)]}',");
+                sb.Append($@"'{Random.Shared.Next(1, 30)}',");
+                sb.Append($@"'{_month[Random.Shared.Next(_month.Length)]}',");
+                sb.Append($@"'{_year[Random.Shared.Next(_year.Length)]}',");
+                sb.Append($@"'{_ct.GetTime()}',");
+                sb.Append($@"'{_puzzle[Random.Shared.Next(_puzzle.Length)]}',");
+                sb.Append($@"'{Random.Shared.Next(1, 15)}',");
+                sb.Append($@"'{_text_used[Random.Shared.Next(_text_used.Length)]}',");
+                sb.Append($@"'{_glitch[Random.Shared.Next(_glitch.Length)]}',");
+                sb.Append($@"'{_bypass[Random.Shared.Next(_bypass.Length)]}');{Environment.NewLine}");
+
+                var input = sb.ToString();
+
+                _input.Add(input);
+
+                start++;
+            }
+
+            
+        }
+
+
+    }
+
+    public class CurrentTime
+    {
+        public CurrentTime(DateTime now)
+        {
+            Day = now.Day.ToString();
+            Month = now.ToString("MMMM");
+            Year = now.Year.ToString("0000");
+            DayOfWeek = now.DayOfWeek.ToString();
+            Time = $@"{now.Hour:00}:{now.Minute:00}:{now.Second:00}";
+            _now= now;
+        }
+
+        public DateTime _now;
+        public string Day { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public string DayOfWeek { get; set; }
+        public string Time { get; set; }
+
+        public string GetTime()
+        {
+            return $@"{_now.AddHours(Random.Shared.Next(0, 12)).Hour:00}:{_now.AddMinutes(Random.Shared.Next(0, 59)).Minute:00}:{_now.AddSeconds(Random.Shared.Next(0, 59)).Second:00}";
+        }
+
+    }
+}
