4943f52f4229e6f915d5743503b06dfd0808384b
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-05-07 15:51:53 -0700

    added CMD runner class

---
+++ CommandPromptParse/CmdPromptParse.cs
@@ -0,0 +1,54 @@
+﻿
+// parses CMD prompts to run from a C# application
+
+using System.Diagnostics;
+
+namespace CommandPromptParse
+{
+    internal class CmdPromptParse
+    {
+        static void Main(string[] args)
+        {
+
+            var startInfo = new ProcessStartInfo
+            {
+                //FileName = @"C:\Users\Lawrence\Desktop\play_vid.bat",
+                FileName = "cmd.exe",
+                RedirectStandardInput = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                //Arguments = "/C ipconfig & Query session & cd C:\\Users\\Lawrence\\Downloads\\PSTools & dir & ",
+                //Arguments = "/C hostname",
+                Arguments = 
+                    // "/C cd C:\\Users\\Lawrence\\Desktop\\ " +
+                "/C start vlc.exe --fullscreen intro_video.mp4 " +
+                "& timeout /T 10" +
+                "& taskkill /IM vlc.exe /F",
+
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            };
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+            // Now read the value, parse to int and add 1 (from the original script)
+            string online = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(online);
+
+            //var activeLoc = online.IndexOf("Active");
+
+            //int i = activeLoc - 3;
+
+            //string sessionId = online.Substring(i, 3);
+            //sessionId.Trim();
+
+
+            Console.WriteLine("Session ID: " + online);
+
+            process.CloseMainWindow();
+            //Environment.Exit(0);
+
+        }
+    }
+}
---
+++ CommandPromptParse/Command_Runner.cs
@@ -0,0 +1,101 @@
+﻿
+// works with "PanIQVideoPlayer_V2" ClientSlave app
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Command_Runner
+    {
+        public void StartIntroVideo()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = false,  // setting this to 'false' allows for 'timeout /T' to work as arg
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                Arguments = "/C start vlc.exe --fullscreen --no-video-title-show --no-qt-fs-controller intro_video.mp4 " +
+                            "& timeout /T 10" +
+                            "& taskkill /IM vlc.exe /F",
+
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            };
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+
+
+            process.CloseMainWindow();
+
+        }
+
+        public void StopIntroVideo()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = false,  // setting this to 'false' allows for 'timeout /T' to work as arg
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                Arguments = "/C taskkill /IM vlc.exe",
+
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized,
+            };
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+
+            process.CloseMainWindow();
+        }
+
+
+        // Takes in a <List> of CMD commands, parses to a string
+        // and then runs the commands
+
+        public string RunCommandWithReturn(List<string> command)
+        {
+            string result = string.Empty;
+            string commands = string.Empty;
+
+            foreach (var item in command)
+            {
+                commands += item;
+                commands += " & ";
+            }
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = false,  // setting this to 'false' allows for 'timeout /T' to work as arg
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                Arguments = commands,
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            };
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+
+            result = process.StandardOutput.ReadToEnd();
+            result = result.ToString().TrimEnd('\r', '\n');
+
+            //process.CloseMainWindow();
+
+            return result;
+
+        }
+    }
+}
